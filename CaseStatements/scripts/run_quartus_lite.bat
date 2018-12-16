eb863c21ce2cb2ff67a7c72d1f265ca852e5b46f
Stefan Mayrhofer <s1610306022@students.fh-hagenberg.at>
2018-12-16 07:55:32 +0100

    finalize synthesis with quartus light

--- CaseStatements/scripts/run_quartus_lite.bat
+++ CaseStatements/scripts/run_quartus_lite.bat
@@ -5,9 +5,6 @@
 
 SET retval=0
 
-rem SET TBNames=tbCounterWithReset
-rem SET quartusLitePath=%MY_QUARTUS_PATH%\quartus_sh.exe
-
 for %%i in (%TBNames%) do (
 	%quartusLitePath% --flow compile %%i || CALL %Checkerror% "Error in quartus lite workflow"
 )
