6a83d0440c0f0164ed184402c8a5e64cd65ca270
Stefan Mayrhofer <s1610306022@students.fh-hagenberg.at>
2019-02-26 12:46:31 +0100

    added testbench

---
+++ LogicalRelationalOperators/tbdLogicalOperators.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tbdLogicalOperators is
+	port(
+		SW : in std_ulogic_vector(9 downto 0);
+		LEDR : out std_ulogic_vector(9 downto 0)
+	);
+end entity tbdLogicalOperators;
+
+architecture RTL of tbdLogicalOperators is
+	constant cWidth : natural := 9;
+begin
+	uut_AND_Classic: entity work.LogicalOperators(AND_Classic)
+	generic map (gWidth => cWidth)
+	port map(
+		iData   => SW,
+		oResult => LEDR(1)
+	);
+		
+	uut_AND_Modern: entity work.LogicalOperators(AND_Modern)
+		generic map(
+			gWidth => cWidth
+		)
+		port map(
+			iData   => SW,
+			oResult => LEDR(0)
+		);
+end architecture RTL;
