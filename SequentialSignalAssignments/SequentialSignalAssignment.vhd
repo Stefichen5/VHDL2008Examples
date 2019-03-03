f96469636397e380ba4ab279a1e1059f33d5dade
Stefan Mayrhofer <s1610306022@students.fh-hagenberg.at>
2019-03-03 08:05:26 +0100

    added clearer Pre-VHDL-2008-Example

--- SequentialSignalAssignments/SequentialSignalAssignment.vhd
+++ SequentialSignalAssignments/SequentialSignalAssignment.vhd
@@ -14,9 +14,9 @@ end entity SequentialSignalAssignment;
 
 architecture MuxExample_New of SequentialSignalAssignment is	
 begin
+	--Signal assignments within a process are only allowed in 08
 	Multiplex : process (iA, iB, iCtrl) is
 	begin
-		--Signal assignments within a process are only allowed in 08
 		oRes <= iA when iCtrl else iB;
 	end process;
 end architecture MuxExample_New;
@@ -25,5 +25,15 @@ architecture MuxExample_Old of SequentialSignalAssignment is
 begin
 	--in older versions of VHDL, sequential signal assignments
 	--can not be within a process
-	oRes <= iA when iCtrl='1' else iB;
+--	oRes <= iA when iCtrl='1' else iB;
+	
+	--or within a process with if-else
+	Multiplex: process(iA, iB, iCtrl) is
+	begin
+		if(iCtrl='1') then
+			oRes <= iA;
+		else
+			oRes <= iB;
+		end if;
+	end process;
 end architecture MuxExample_Old;
