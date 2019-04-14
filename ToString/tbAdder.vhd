a59a8eea32bf92cfa9a9119c978f77937afa4f3a
Stefan Mayrhofer <s1610306022@students.fh-hagenberg.at>
2019-04-14 10:58:25 +0200

    added examples for custom types

--- ToString/tbAdder.vhd
+++ ToString/tbAdder.vhd
@@ -12,6 +12,11 @@ architecture Bhv of tbAdder is
 	
 	signal iClk, inResAsync : std_ulogic := '0';
 	signal iA, iB, oRes : natural := 0;
+	
+	type aCustomType is (ValueA, ValueB, ValueC);
+	signal CustomSignal: aCustomType := ValueA;
+	signal ulogicbit : std_ulogic := '1';
+	signal ulogicvect : std_ulogic_vector(7 downto 0) := "11111111";
 begin
 	UUT: entity work.Adder
 		port map(
@@ -33,7 +38,18 @@ begin
 		wait until oRes'EVENT;
 		
 		assert oRes = 3 report to_string(iA) & " + " & to_string(iB) & " is " & to_string(oRes) & " but should be 3" severity error;
-	
+			
 		finish;
+	end process;
+	
+	--use to_string for different data types
+	print: process is
+	begin
+		report to_string(CustomSignal);
+		report to_string(ulogicbit);
+		report to_string(ulogicvect);
+		report to_hstring(ulogicvect);
+		report to_ostring(ulogicvect);		
+		wait;
 	end process;
 end architecture Bhv;
