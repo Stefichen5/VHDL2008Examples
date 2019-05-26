85b75280ed589d2ac39381d5380b429f1db5c51e
Stefan Mayrhofer <s1610306022@students.fh-hagenberg.at>
2019-05-26 19:12:35 +0200

    fixed

--- CaseStatements/ComparisonOldNewCase.vhd
+++ CaseStatements/ComparisonOldNewCase.vhd
@@ -1,3 +1,7 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 entity testcomparison is
 end entity testcomparison;
 
@@ -7,26 +11,27 @@ begin
 	stimul: process is
 	begin
 		my_signal <= '-';
+		wait;
 	end process;
 
 	regular_case: process is
 	begin
 		case my_signal is
-			when "-" => report "matching case: don't care detected";
-			when others => report "matching case: other value detected";
+			when '-' => report "regular case: don't care detected";
+			when others => report "regular case: other value detected";
 		end case;
 
-		wait;
+		wait until my_signal'EVENT;
 	end process;
 
 	matching_case: process is
 	begin
 		case? my_signal is
-			when "-" => report "matching case: don't care detected";
+			when '-' => report "matching case: don't care detected";
 			when others => report "matching case: other value detected";
 		end case?;
 
-		wait;
+		wait until my_signal'EVENT;
 	end process;
 
 end architecture Bhv;
