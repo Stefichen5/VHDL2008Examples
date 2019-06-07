3753ff9f64264554ee0ea34404d4b7d479c5f0f6
Stefan Mayrhofer <s1610306022@students.fh-hagenberg.at>
2019-06-07 08:50:39 +0200

    simple example for testing, which value is beeing overwrritten (driver or actual value)

---
+++ ForceRelease/forcereleasetest.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use std.env.all;
+
+entity forcereleasetest is
+end entity forcereleasetest;
+
+architecture Bhv of forcereleasetest is
+	signal mySignal : std_ulogic := '0';
+begin
+	
+	Stimul : process is
+	begin
+		mySignal <= '1' after 5 ns, '0' after 10 ns, '1' after 20 ns;
+		wait for 50 ns;
+		finish;
+	end process;
+
+	ForceTest: process is
+	begin
+		wait for 6 ns;
+		mySignal <= force 'X';
+		wait;
+	end process;
+
+end architecture Bhv;
